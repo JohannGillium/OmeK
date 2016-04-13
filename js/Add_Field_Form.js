6b1e3322034d5946c3233d604178d4feac61cc21
JohannGillium <johanngillium@gmail.com>
2016-04-13 12:29:33 -0400

    Enhancement of the dynamically addition of fields, with the same model of field everywhere.

--- js/Add_Field_Form.js
+++ js/Add_Field_Form.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     $("#add").click(function() {
-        var intId = $("#buildyourform div").length + 1;
+        var intId = $("#form_fields div").length + 1;
         var fieldWrapper = $("<div class=\"fieldwrapper\" id=\"field" + intId + "\"/>");
-        var fName = $("<input type=\"text\" class=\"fieldname\" />");
-        var fType = $("<select class=\"fieldtype\"><option value=\"checkbox\">Checked</option><option value=\"textbox\">Text</option><option value=\"textarea\">Paragraph</option></select>");
+        var fName = $("<div id=\"search-keywords\" class=\"field\"><label for=\"keyword-search\">Search for Keywords</label>       <div class=\"inputs\"><input type=\"text\" name=\"search\" id=\"keyword-search\" value=\"\" size=\"40\">        </div></div>");
+        var fType = $("<div id=\"search-narrow-by-fields\" class=\"field\"><div class=\"label\">Narrow by Specific Fields</div><div class=\"inputs\"><div class=\"search-entry\"><select title=\"Search Field\" class=\"advanced-search-element\"><option value=\"\">Select Below </option><optgroup id=\"optgroupDublinCore\" label=\"Dublin Core\"><option value=\"37\">contributor</option><option value=\"38\">coverage</option><option value=\"39\">creator</option><option value=\"40\">date</option><option value=\"41\">description</option><option value=\"42\">format</option><option value=\"43\">identifier</option><option value=\"44\">language</option><option value=\"45\">publisher</option><option value=\"46\">relation</option><option value=\"47\">rights</option><option value=\"48\">source</option><option value=\"49\">subject</option><option value=\"50\">title</option><option value=\"51\">type</option></optgroup></select></div></div></div></div>");
         var removeButton = $("<input type=\"button\" class=\"remove\" value=\"-\" />");
         removeButton.click(function() {
             $(this).parent().remove();
